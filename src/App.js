6fbafc7896c4c8b4e363286c05dd20d64bb17c8a
MirzaMuhammadBaig <mirzamuhammadbaig328@gmail.com>
2023-09-20 21:38:40 +0500

    project-initialization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import LeftLine from './components/LeftLine';
-import Leftcard from './components/Leftcard';
-import TopCards from './components/TopCards';
+import React from "react";
+import LeftLine from "./component/LeftLine";
+import TopCards from "./component/TopCards";
+import BelowCards from "./component/BelowCards";
 
 function App() {
   return (
-    <div className="" style={{backgroundColor:"#2a2649"}}>
-      <LeftLine/>
+    <div className="min-h-screen flex" style={{ backgroundColor: "#2a2649" }}>
+      <LeftLine />
+      <div className="w-[98vw] p-8">
+        <TopCards />
+        <BelowCards />
+      </div>
     </div>
   );
 }
