be7fc07f91a1d44cdda22bac4b7717a918208d29
MirzaMuhammadBaig <mirzamuhammadbaig328@gmail.com>
2023-09-20 18:00:07 +0500

    project-start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import LeftLine from './components/LeftLine';
+import Leftcard from './components/Leftcard';
+import TopCards from './components/TopCards';
+
+function App() {
+  return (
+    <div className="" style={{backgroundColor:"#2a2649"}}>
+      <LeftLine/>
+    </div>
+  );
+}
+
+export default App;
